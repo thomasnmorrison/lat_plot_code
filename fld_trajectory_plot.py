cc01ba705ec9d1d99a07ec8885ca263900a9e90e
thomasnmorrison <morrison@physica.utoronto.ca>
2020-10-09 12:03:54 -0400

    wrote the resizing of arrays

--- fld_trajectory_plot.py
+++ fld_trajectory_plot.py
@@ -15,15 +15,15 @@ import matplotlib.pyplot as plt
 # File names and paths
 path_n = '../lattice-dev-master/Pseudospec/openmp_dev/'
 en_bl_f = 'energy_spec_TESTING_.out' # Baseline run lattice averaged quantities
-en_f = [] # lattice averaged quatities
+en_f = ['energy_spec_TESTING_.out'] # lattice averaged quatities
 phi_bl_f = 'phi_lat_TESTING_.out'
-phi_f = []
+phi_f = ['phi_lat_TESTING_.out']
 dphi_bl_f = 'dphi_lat_TESTING_.out'
-dphi_f = []
+dphi_f = ['dphi_lat_TESTING_.out']
 chi_bl_f = 'chi_lat_TESTING_.out'
-chi_f = []
+chi_f = ['chi_lat_TESTING_.out']
 dchi_bl_f = 'dchi_lat_TESTING_.out'
-dchi_f = []
+dchi_f = ['dchi_lat_TESTING_.out']
 
 # Run parameters
 nx = 8; ny = 8; nz = 8
@@ -52,21 +52,24 @@ phi = np.zeros((np.shape(phi_f) + np.shape(phi_bl)))
 dphi = np.zeros((np.shape(dphi_f) + np.shape(dphi_bl)))
 
 for i in range(0,len(en_f)):
-	en[i] = np.fromfile(path_n+en_f[i], dtype=np.double , count=-1)
+	en[i] = np.loadtxt(path_n+en_f[i])
 	phi[i] = np.fromfile(path_n+phi_f[i], dtype=np.double , count=-1)
+	dphi[i] = np.fromfile(path_n+dphi_f[i], dtype=np.double , count=-1)
 
 if (chi_bl_f != ''):
 	chi_bl = np.fromfile(path_n+chi_bl_f, dtype=np.double , count=-1)
 	dchi_bl = np.fromfile(path_n+dchi_bl_f, dtype=np.double , count=-1)
 	chi = np.zeros((np.shape(chi_f) + np.shape(chi_bl)))
 	dchi = np.zeros((np.shape(dchi_f) + np.shape(chi_bl)))
-	for i in range(0,len(en_f)):
+	for i in range(0,len(chi_f)):
 		chi[i] = np.fromfile(path_n+chi_f[i], dtype=np.double , count=-1)
 		dchi[i] = np.fromfile(path_n+dchi_f[i], dtype=np.double , count=-1)
  
 # Reshape lattice data
 print('np.shape(phi_bl) = ', np.shape(phi_bl))
 print('np.shape(en_bl) = ', np.shape(en_bl))
+print('np.shape(phi) = ', np.shape(phi))
+print('np.shape(en) = ', np.shape(en))
 nlat = nx*ny*nz
 t =  int(len(en_bl[:,0]-1)/sl) + 1
 print('t = ', t)
@@ -76,8 +79,17 @@ if (chi_bl_f != ''):
 	chi_bl = np.resize(chi_bl,(t,nlat)); chi_bl = chi_bl.transpose()     # Formatted [lat site, time]
 	dchi_bl = np.resize(dchi_bl,(t,nlat)); dchi_bl = dchi_bl.transpose() # Formatted [lat site, time]
 
+if (len(phi_f) > 0):
+	phi = np.resize(phi, (len(phi_f),t,nlat)); phi = phi.transpose((0,2,1))       # Formatted [run, lat site, time]
+	dphi = np.resize(dphi, (len(dphi_f),t,nlat)); dphi = dphi.transpose((0,2,1))  # Formatted [run, lat site, time]
+if (len(chi_f) > 0):
+	chi = np.resize(chi, (len(chi_f),t,nlat)); chi = chi.transpose((0,2,1))       # Formatted [run, lat site, time]
+	dchi = np.resize(dchi, (len(dchi_f),t,nlat)); dchi = dchi.transpose((0,2,1))  # Formatted [run, lat site, time]
+
+
 print('np.shape(phi_bl) = ', np.shape(phi_bl))
-#print('phi_bl[:,1] = ', phi_bl[:,1])
+print('np.shape(phi) = ', np.shape(phi))
+print('np.shape(en) = ', np.shape(en))
 
 
 # Indexing constants
@@ -94,44 +106,44 @@ f_title = r'$\phi-\bar{\phi}$'
 s_title = [r'$\delta\phi$',r'$\Delta\phi$']
 x_lab = [r'$\mathrm{ln}(a)$', r'$\mathrm{ln}(a)$']
 y_lab = [r'$M_{Pl}^{-1}$', r'$M_{Pl}^{-1}$']
-fig.suptitle(f_title, fontsize = title_fs)
+#fig.suptitle(f_title, fontsize = title_fs)
 for i in range(0,2):
+	ax[i].set_title(s_title[i], fontsize = stitle_fs)
 	ax[i].set_xlabel(x_lab[i], fontsize = x_lab_fs)
 	ax[i].set_ylabel(y_lab[i], fontsize = y_lab_fs)
 	ax[i].ticklabel_format(axis='y', style='scientific', scilimits=(0,0))
 
-delta_f = phi_bl[:,:]-en_bl[::sl,phi_i]
-ax[0].plot(np.log(en_bl[::sl,a_i]), delta_f.T)
+ax[0].plot(np.log(en_bl[::sl,a_i]), (phi_bl[:,:]-en_bl[::sl,phi_i]).T)
 for i in range(0,len(phi_f)):
-        ax[0].plot(np.log(en[i,::s,a_i]), (phi[i,:,:]-en[i,::sl,phi_i]).T)
+        ax[0].plot(np.log(en[i,::sl,a_i]), (phi[i,:,:]-en[i,::sl,phi_i]).T)
         ax[1].plot(np.log(en_bl[::sl,a_i]), (phi[i,:,:]-phi_bl[:,:]).T)
 
+fig.set_tight_layout(True)
 if (SCALE_FIG == True):
 	fig.set_size_inches(FIG_SIZE)
 if (SAVE_FIG[nfig-1] == True):
 	fig.savefig()
         
-# Plot 2: \delta\dot{\phi} clocked on \alpha, \Delta\Pi_\phi clocked on \alpha 
+# Plot 2: \delta\dot{\phi} clocked on \alpha, \Delta\dot{\phi} clocked on \alpha 
 nfig += 1
 fig, ax = plt.subplots(nrows=2 , ncols=1 , sharex=False)
 f_title = r'$\phi-\bar{\phi}$'
-s_title = [r'$\phi-\bar{\phi}$', r'$\phi-\bar{\phi}$']
+s_title = [r'$\delta\dot{\phi}$', r'$\Delta\dot{\phi}$']
 x_lab = [r'$\mathrm{ln}(a)$', r'$\mathrm{ln}(a)$']
-y_lab = [r'$\delta\dot{\phi} m_\phi^{-1}M_{Pl}^{-1}$', r'$\delta\dot{\phi} m_\phi^{-1}M_{Pl}^{-1}$']
-fig.suptitle(f_title, fontsize = title_fs)
+y_lab = [r'$(m_\phi M_{Pl})^{-1}$', r'$(m_\phi M_{Pl})^{-1}$']
+#fig.suptitle(f_title, fontsize = title_fs)
 for i in range(0,2):
+	ax[i].set_title(s_title[i], fontsize = stitle_fs)
 	ax[i].set_xlabel(x_lab[i], fontsize = x_lab_fs)
 	ax[i].set_ylabel(y_lab[i], fontsize = y_lab_fs)
 	ax[i].ticklabel_format(axis='y', style='scientific', scilimits=(0,0))
 
-delta_f = (dphi_bl[:,:]-en_bl[::sl,dphi_i])/en_bl[::sl,a_i]**3
-delta_f = delta_f.transpose() # Formatted [time, lat site]
-ax[0].plot(np.log(en_bl[::sl,a_i]), delta_f)
+ax[0].plot(np.log(en_bl[::sl,a_i]), ((dphi_bl[:,:]-en_bl[::sl,dphi_i])/en_bl[::sl,a_i]**3).T)
 for i in range(0,len(dphi_f)):
-        ax[0].plot(np.log(en[i,::sl,a_i]), (dphi[i,:,:]-en[i,::sl,dphi_i]).T/en[i,::sl,a_i]**3)
-ax[1].plot(en_bl[::sl,phi_i], delta_f)
-ax[1].invert_xaxis()
+	ax[0].plot(np.log(en[i,::sl,a_i]), ((dphi[i,:,:]-en[i,::sl,dphi_i])/en[i,::sl,a_i]**3).T)
+	ax[1].plot(np.log(en_bl[::sl,a_i]), (dphi[i,:,:]/en[i,::sl,a_i]**3-dphi_bl[:,:]/en_bl[::sl,a_i]**3).T)
 
+fig.set_tight_layout(True)
 if (SCALE_FIG == True):
 	fig.set_size_inches(FIG_SIZE)
 if (SAVE_FIG[nfig-1] == True):
@@ -142,20 +154,22 @@ if (chi_bl_f != ''):
 	nfig += 1
 	fig, ax = plt.subplots(nrows=2 , ncols=1 , sharex=False)
 	f_title = r'$\chi-\bar{\chi}$'
-	s_title = [r'$\chi-\bar{\chi}$', r'$\chi-\bar{\chi}$']
-	x_lab = [r'$\mathrm{ln}(a)$', r'$\bar{\phi}M_{Pl}^{-1}$']
-	y_lab = [r'$\delta\chi M_{Pl}^{-1}$', r'$\delta\chi M_{Pl}^{-1}$']
-	fig.suptitle(f_title, fontsize = title_fs)
+	s_title = [r'$\delta\chi$',r'$\Delta\chi$']
+	x_lab = [r'$\mathrm{ln}(a)$', r'$\mathrm{ln}(a)$']
+	y_lab = [r'$M_{Pl}^{-1}$', r'$M_{Pl}^{-1}$']
+	#fig.suptitle(f_title, fontsize = title_fs)
 	for i in range(0,2):
+		ax[i].set_title(s_title[i], fontsize = stitle_fs)
 		ax[i].set_xlabel(x_lab[i], fontsize = x_lab_fs)
 		ax[i].set_ylabel(y_lab[i], fontsize = y_lab_fs)
 		ax[i].ticklabel_format(axis='y', style='scientific', scilimits=(0,0))
 
-	delta_f = chi_bl[:,:]-en_bl[::sl,chi_i]#; delta_f = delta_f.transpose() # Formatted [time, lat site]
-	ax[0].plot(np.log(en_bl[::sl,a_i]), delta_f.T)
-	ax[1].plot(en_bl[::sl,phi_i], delta_f.T)
-	ax[1].invert_xaxis()
+	ax[0].plot(np.log(en_bl[::sl,a_i]), (chi_bl[:,:]-en_bl[::sl,chi_i]).T)
+	for i in range(0,len(chi_f)):
+		ax[0].plot(np.log(en[i,::sl,a_i]), (chi[i,:,:]-en[i,::sl,chi_i]).T)
+		ax[1].plot(np.log(en_bl[::sl,a_i]), (chi[i,:,:]-chi_bl[:,:]).T)
 
+	fig.set_tight_layout(True)
 	if (SCALE_FIG == True):
 		fig.set_size_inches(FIG_SIZE)
 	if (SAVE_FIG[nfig-1] == True):
@@ -165,21 +179,23 @@ if (dchi_bl_f != ''):
 # Plot 4: \delta\Pi_phi clocked on \phi, \delta\Pi_\phi clocked on \alpha
 	nfig += 1
 	fig, ax = plt.subplots(nrows=2 , ncols=1 , sharex=False)
-	f_title = r'$\dot{\chi}-\dot{\bar{\chi}}$'
-	s_title = [r'$\dot{\chi}-\dot{\bar{\chi}}$', r'$\dot{\chi}-\dot{\bar{\chi}}$']
-	x_lab = [r'$\mathrm{ln}(a)$', r'$\bar{\phi}M_{Pl}^{-1}$']
-	y_lab = [r'$\delta\chi M_{Pl}^{-1}$', r'$\delta\chi M_{Pl}^{-1}$']
-	fig.suptitle(f_title, fontsize = title_fs)
+	f_title = r'$\chi-\bar{\chi}$'
+	s_title = [r'$\delta\dot{\chi}$', r'$\Delta\dot{\chi}$']
+	x_lab = [r'$\mathrm{ln}(a)$', r'$\mathrm{ln}(a)$']
+	y_lab = [r'$(m_\phi M_{Pl})^{-1}$', r'$(m_\phi M_{Pl})^{-1}$']
+	#fig.suptitle(f_title, fontsize = title_fs)
 	for i in range(0,2):
+		ax[i].set_title(s_title[i], fontsize = stitle_fs)
 		ax[i].set_xlabel(x_lab[i], fontsize = x_lab_fs)
 		ax[i].set_ylabel(y_lab[i], fontsize = y_lab_fs)
 		ax[i].ticklabel_format(axis='y', style='scientific', scilimits=(0,0))
 
-	delta_f = (dchi_bl[:,:]-en_bl[::sl,dchi_i])/en_bl[::sl,a_i]**3        # Formatted [time, lat site]
-	ax[0].plot(np.log(en_bl[::sl,a_i]), delta_f.T)
-	ax[1].plot(en_bl[::sl,phi_i], delta_f.T)
-	ax[1].invert_xaxis()
+	ax[0].plot(np.log(en_bl[::sl,a_i]), ((dchi_bl[:,:]-en_bl[::sl,dchi_i])/en_bl[::sl,a_i]**3).T)
+	for i in range(0,len(dchi_f)):
+		ax[0].plot(np.log(en[i,::sl,a_i]), ((dchi[i,:,:]-en[i,::sl,dchi_i])/en[i,::sl,a_i]**3).T)
+		ax[1].plot(np.log(en_bl[::sl,a_i]), (dchi[i,:,:]/en[i,::sl,a_i]**3-dchi_bl[:,:]/en_bl[::sl,a_i]**3).T)
 
+	fig.set_tight_layout(True)
 	if (SCALE_FIG == True):
 		fig.set_size_inches(FIG_SIZE)
 	if (SAVE_FIG[nfig-1] == True):
